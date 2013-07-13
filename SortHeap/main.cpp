a63307995684fce784c225550481be19882fcaec
Eagle_Han <tian19880220@126.com>
2013-07-13 13:36:37 +0800

    SortHeap
    A tool for get top k items from a large set. A template one

--- SortHeap/main.cpp
+++ SortHeap/main.cpp
@@ -7,12 +7,33 @@
 //
 
 #include <iostream>
+#include "SortHeap.h"
+
+using namespace std;
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int MaxSize = 7;
+    SortHeap<double> sort(MaxSize);
+    sort.Insert(0.12);
+    sort.Insert(0.35);
+    sort.Insert(0.413);
+    sort.Insert(0.59);
+    sort.Insert(0.002);
+    sort.Insert(0.11);
+    sort.Insert(0.59);
+    sort.Insert(0.41);
+    sort.Insert(0.22);
+    sort.Insert(0.85);
+    sort.Insert(0.32);
+    sort.Insert(0.8);
+    sort.Insert(1.5);
+    sort.Insert(2.6);
+    double* heap = sort.Output();
+    for (int i=1;i<MaxSize+1;i++){
+        cout<<heap[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
 
